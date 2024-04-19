7ebc29b103ec4cfb4eff2cb69593c17be4a96b56
hushpuppiesss <rachelquedding1611@gmail.com>
2024-04-18 22:58:32 -0400

    update

--- stations/eggCrate.gd
+++ stations/eggCrate.gd
@@ -3,17 +3,16 @@ extends StaticBody2D
 @onready var interaction_area: InteractionArea = $InteractionArea
 @onready var sprite = $Sprite2D
 
+@export var egg : PackedScene
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	interaction_area.interact = Callable(self, "_get_egg")
 
 func _get_egg():
 	
-	print("egg")
-	#sprite.frame = 1 if sprite.frame == 0 else 0
-	
-	#for n in range(6):
-		#sprite.frame = n
+	var newInstance = egg.instantiate()
+	add_child(newInstance)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
