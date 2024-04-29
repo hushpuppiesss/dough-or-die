68514c63c7b03005c651c3f05a7ba8f41aaf0da9
hushpuppiesss <rachelquedding1611@gmail.com>
2024-04-29 02:43:35 -0400

    WE COOKED!!!
    now the scoring systme

--- stations/cooks/fryer.gd
+++ stations/cooks/fryer.gd
@@ -2,14 +2,66 @@ extends StaticBody2D
 
 @onready var interaction_area: InteractionArea = $InteractionArea
 @onready var sprite = $Sprite2D
+@onready var progress_bar = $"progress bar"
+
+# spits out dough
+@export var dough_cooked : PackedScene
+
+# icons
+@onready var raw_dough_icon = $"raw dough icon"
+@onready var raw_dough_anim = $"raw dough icon/AnimationPlayer"
+
+
+# keeps track of the ingredients inside
+@onready var ingredients_in = []
+
+# if its full or empty
+@onready var loaded = false
+
+# if its done mixing
+@onready var mixed = false
+
+@onready var player = InteractionManager.player
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	interaction_area.interact = Callable(self, "_fry_donuts")
-
-func _fry_donuts():
-	pass
+	interaction_area.interact = Callable(self, "_fry")
+	raw_dough_anim.play("float")
+	progress_bar.hide()
+	
+func _fry():
+	if loaded == false:
+		# check if player has an ingredient to put in
+		if CookingManager.carrying:
+			if CookingManager.item_in_hand.is_in_group("dough raw") && !ingredients_in.has("dough raw"):
+				# adds ingredient to array
+				ingredients_in.append(CookingManager.item_in_hand.name)
+				CookingManager._put_down()
+				CookingManager.item_in_hand.queue_free()
+				raw_dough_icon.visible = false
+				
+				#print(ingredients_in)
+				
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	# checking ingredients
+	if ingredients_in.has("dough raw"):
+		loaded = true
+		progress_bar.show()
+		$Timer.start()
+		ingredients_in.clear()
+		
+	progress_bar.value = $Timer.time_left
+	
+func _on_timer_timeout():
+	mixed = true
+	loaded = false
+	
+	raw_dough_icon.visible = true
+	progress_bar.hide()
+	
+	var newInstance = dough_cooked.instantiate()
+	add_child(newInstance)
+	newInstance.position.x += 32
+	newInstance.position.y += 48
--- stations/cooks/toppings.gd
+++ stations/cooks/toppings.gd
@@ -5,15 +5,12 @@ extends StaticBody2D
 @onready var progress_bar = $"progress bar"
 
 # spits out dough
-@export var raw_dough : PackedScene
+@export var doughnut: PackedScene
 
 # icons
-@onready var wheat_icon = $"wheat icon"
-@onready var wheat_anim = $"wheat icon/AnimationPlayer"
-@onready var milk_icon = $"milk icon"
-@onready var milk_anim = $"milk icon/AnimationPlayer"
-@onready var egg_icon = $"egg icon"
-@onready var egg_anim = $"egg icon/AnimationPlayer"
+@onready var raw_dough_icon = $"raw dough icon"
+@onready var raw_dough_anim = $"raw dough icon/AnimationPlayer"
+
 
 # keeps track of the ingredients inside
 @onready var ingredients_in = []
@@ -28,47 +25,33 @@ extends StaticBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	interaction_area.interact = Callable(self, "_mix")
-	wheat_anim.play("float")
-	milk_anim.play("float")
-	egg_anim.play("float")
+	interaction_area.interact = Callable(self, "_fry")
+	raw_dough_anim.play("float")
 	progress_bar.hide()
 	
-func _mix():
+func _fry():
 	if loaded == false:
 		# check if player has an ingredient to put in
 		if CookingManager.carrying:
-			if CookingManager.item_in_hand.is_in_group("flour") && !ingredients_in.has("flour"):
+			if CookingManager.item_in_hand.is_in_group("dough cooked") && !ingredients_in.has("dough cooked"):
 				# adds ingredient to array
 				ingredients_in.append(CookingManager.item_in_hand.name)
 				CookingManager._put_down()
 				CookingManager.item_in_hand.queue_free()
-				wheat_icon.visible = false
+				raw_dough_icon.visible = false
 				
 				#print(ingredients_in)
-				
-			if CookingManager.item_in_hand.is_in_group("egg") && !ingredients_in.has("egg"):
+			if CookingManager.item_in_hand.is_in_group("chocolate") && !ingredients_in.has("chocolate"):
+				# adds ingredient to array
 				ingredients_in.append(CookingManager.item_in_hand.name)
 				CookingManager._put_down()
 				CookingManager.item_in_hand.queue_free()
-				egg_icon.visible = false
-				
-				#print(ingredients_in)
-				
-			if CookingManager.item_in_hand.is_in_group("milk") && !ingredients_in.has("milk"):
-				ingredients_in.append(CookingManager.item_in_hand.name)
-				CookingManager._put_down()
-				CookingManager.item_in_hand.queue_free()
-				milk_icon.visible = false
-				
-				#print(ingredients_in)
-				
 				
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	# checking ingredients
-	if ingredients_in.has("egg") && ingredients_in.has("milk") && ingredients_in.has("flour"):
+	if ingredients_in.has("dough cooked") && ingredients_in.has("chocolate"):
 		loaded = true
 		progress_bar.show()
 		$Timer.start()
@@ -80,13 +63,10 @@ func _on_timer_timeout():
 	mixed = true
 	loaded = false
 	
-	wheat_icon.visible = true
-	milk_icon.visible = true
-	egg_icon.visible = true
+	raw_dough_icon.visible = true
 	progress_bar.hide()
-	print("rah")
 	
-	var newInstance = raw_dough.instantiate()
+	var newInstance = doughnut.instantiate()
 	add_child(newInstance)
-	newInstance.position.x += 32
-	newInstance.position.y += 16
+	newInstance.position.x += 48
+	newInstance.position.y += 0
